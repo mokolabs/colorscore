6a1794a03d9c431507b04b72f41d98a264253854
Joakim Nylén <me@jnylen.nu>
2016-08-09 00:39:00 +0200

    Fix tests

--- test/histogram_test.rb
+++ test/histogram_test.rb
@@ -3,12 +3,12 @@ require File.expand_path("../test_helper", __FILE__)
 class HistogramTest < Test::Unit::TestCase
   def test_color_count_is_correct
     colors = 7
-    histogram = Histogram.new("test/fixtures/skydiver.jpg", colors)
-    assert_operator histogram.colors.size, :<=, colors
+    histogram = Histogram.new("test/fixtures/skydiver.jpg", :colors => colors)
+    assert_operator histogram.hex_colors.size, :<=, colors
   end
 
   def test_transparency_is_ignored
     histogram = Histogram.new("test/fixtures/transparency.png")
-    assert_equal Color::RGB.from_html('0000ff'), histogram.colors.first
+    assert_equal Color::RGB.from_html('0000ff'), histogram.hex_colors.first
   end
 end
